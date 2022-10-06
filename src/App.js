c83585ac91de2023005ade37b3a587fc9e0973db
mflamanna <lamanna.mflorencia@gmail.com>
2022-10-06 10:55:26 +0200

    primer commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HomePage from './Components/Home/HomePage';
+
+
+function App() {
+  return (
+    <>
+      <HomePage/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CalendarSetMore from '../Calendario/Calendar'
+
+function HomePage() {
+  return (
+    <div>
+      <p>HomePage</p>
+      <CalendarSetMore/>
+    </div>
+    
+  )
+}
+
+export default HomePage
