fa158bd56b4b4de92233ac3a5fe223f1753df386
mflamanna <lamanna.mflorencia@gmail.com>
2022-10-07 10:39:57 +0200

    changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import ContainerIcons from './ContainerIcons'
 
 function Footer() {
   return (
-    <div className='w-full h-36 bg-verde-ciba rounded-tr-[50px] fixed bottom-0 flex justify-center items-center'>
+    <div className='w-full h-36 bg-verde-ciba rounded-tr-[50px] flex justify-center items-center'>
       <ContainerIcons/>
     </div>
   )
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -9,7 +9,6 @@ function HomePage() {
   return (
     <div>
       <Header/>
-      <CalendarSetMore/>
       <SectionVideo/>
       <Footer/>
     </div>
