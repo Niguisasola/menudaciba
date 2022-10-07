3a38452aaa2e297df02dc4591e35a3be8e09b09f
Nika <nikaguisasola@gmail.com>
2022-10-07 12:50:42 +0200

    form pop up done

--- src/Application/Router.js
+++ src/Application/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import App from "../App";
+import Contact from "../Pages/Contact";
 import ReserveByAge from "../Pages/ReserveByAge";
 import Team from "../Pages/Team";
 
@@ -11,6 +12,7 @@ export default function Router (){
             <Route path='/' element={<App/>}/>
             <Route path='/equipo' element={<Team/>}/>
             <Route path='/reservarPorEdad' element={<ReserveByAge/>}/>
+            <Route path='/contact' element={<Contact/>}/>
         </Routes>
         </BrowserRouter>
     )
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,25 +1,25 @@
 import { Link } from "react-router-dom";
-import Logo from '../Components/Logo'
-function Header(){
-    return(
+import Logo from '../Components/Logo';
+import Buttonpopup from "./Buttonpopup";
+
+function Header() {
+    return (
         <div className="flex justify-between h-56 grid grid-cols-2 gap-4 content-start items-center" >
-            
-             <Logo/>
-            
-                    <div className="flex  h-10 w-1.2  content-between items-center justify-between p-4">
-                        <Link to="/AboutUs">
-                             <p className="basis-1/4 text-2xl 01">Sobre Nosotr@s</p>
-                        </Link>
-                            <Link to="/Team">
-                                <p className="basis-1/2 text-2xl 02">Equipo</p>
-                            </Link>
-                                <Link to="/Calendar">
-                                    <p className="basis-1/4 text-2xl 03">Calendario</p>
-                                </Link>
-                                    <Link to="/Contact">
-                                        <p className="basis-1/2 text-2xl 04">Contacto</p>
-                                    </Link>
-                    </div>
+
+            <Logo />
+
+            <div className="flex  h-10 w-1.2  content-between items-center justify-between p-4">
+                <Link to="/AboutUs">
+                    <p className="basis-1/4 text-2xl 01">Sobre Nosotr@s</p>
+                </Link>
+                <Link to="/Team">
+                    <p className="basis-1/2 text-2xl 02">Equipo</p>
+                </Link>
+                <Link to="/Calendar">
+                    <p className="basis-1/4 text-2xl 03">Calendario</p>
+                </Link>
+                <p className="basis-1/2 text-2xl 04"><Buttonpopup /></p>
+            </div>
         </div>
     )
 }
