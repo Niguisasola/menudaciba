e0d6fae067434f6b645b34b4be7d80684d5c2f81
mflamanna <lamanna.mflorencia@gmail.com>
2022-10-10 15:44:22 +0200

    social links to new tab

--- src/Components/Footer/ContainerIcons.jsx
+++ src/Components/Footer/ContainerIcons.jsx
@@ -6,13 +6,13 @@ import iconTwitter from '../../Assets/Img/Icons/twitter.svg'
 function ContainerIcons() {
   return (
     <div className='display flex justify-between '>
-      <a href="https://www.facebook.com/profile.php?id=100064361934637">
+      <a href="https://www.facebook.com/profile.php?id=100064361934637" target="_blank" rel="noopener noreferrer" >
         <img className='w-10' src={iconFacebook} alt="Facebook" />
       </a>
-      <a href="https://www.instagram.com/lacibascg/">
+      <a href="https://www.instagram.com/lacibascg/" target="_blank" rel="noopener noreferrer" >
         <img className='w-12' src={iconInstagram} alt="Instagram" />
       </a>
-      <a href="https://twitter.com/la_ciba">
+      <a href="https://twitter.com/la_ciba" target="_blank" rel="noopener noreferrer" >
         <img className='w-10' src={iconTwitter} alt="Twitter" />
       </a>
     </div>
