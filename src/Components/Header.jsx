37829fb4e0720f0bbc9b49bbb386c3f80b4d7744
mflamanna <lamanna.mflorencia@gmail.com>
2022-10-10 12:08:46 +0200

    link home ok

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,9 +5,9 @@ import Buttonpopup from "./Buttonpopup";
 function Header() {
     return (
         <div className="flex justify-between h-auto grid grid-cols-2 gap-4 content-start items-center" >
-
+            <Link to='/'>
             <Logo />
-
+            </Link>
             <div className="flex  h-10 w-1.2  content-between items-center justify-between p-4">
                 <Link to="/quienessomos">
                     <p className="shadow bg-violeta-ciba transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-verde-ciba duration-300 focus:shadow-outline focus:outline-none text-white font-koulen py-2 px-4 rounded">Sobre Nosotr@s</p>
