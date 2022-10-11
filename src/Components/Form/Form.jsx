72950128b108cd31fdf6481bb7f3399c4d57b597
mflamanna <lamanna.mflorencia@gmail.com>
2022-10-11 09:23:26 +0200

    form validation

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import { useNavigate } from 'react-router-dom';
 
 function Form() {
 
@@ -23,35 +24,31 @@ function Form() {
 
   const [selected, setSelected] = React.useState("");
 
-  /** Function that will set different values to state variable
-  * based on which dropdown is selected
-  */
   const changeSelectOptionHandler = (event) => {
     setSelected(event.target.value);
   };
 
-  /** Different arrays for different dropdowns */
   const younger = ["9", "10", "11"];
   const older = ["17", "18", "19"];
 
-  /** Type variable to store different array for different dropdown */
   let type = null;
 
-  /** This will be used to create set of options that user will see */
   let options = null;
 
-  /** Setting Type variable according to dropdown */
   if (selected === "0-6") {
     type = younger;
   } else if (selected === "7-13") {
     type = older;
   }
 
-  /** If "Type" is null or undefined then options will be null,
-  * otherwise it will create a options iterable based on our array
-  */
   if (type) {
     options = type.map((el) => <option key={el}>{el}</option>);
+  }
+
+  const navigate = useNavigate();
+
+  const handleClick = () =>{
+    navigate("/")
   }
 
   return (
@@ -59,31 +56,31 @@ function Form() {
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label className="block uppercase tracking-wide text-violeta-ciba text-xs font-bold mb-2" >
-            Nombre
+            Nombre *
           </label>
-          <input required className="appearance-none block w-full bg-violet-100 text-grey-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Pepita" name='nombre' />
+          <input required minlength="3" className="appearance-none block w-full bg-violet-100 text-grey-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Pepita" name='nombre' />
         </div>
         <div className="w-full md:w-1/2 px-3">
           <label className="block uppercase tracking-wide text-violeta-ciba text-xs font-bold mb-2" htmlFor="grid-last-name">
-            Apellido
+            Apellido *
           </label>
-          <input className="appearance-none block w-full bg-violet-100 text-grey-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="La pistolera" name='apellido' />
+          <input required minlength="2" className="appearance-none block w-full bg-violet-100 text-grey-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="La pistolera" name='apellido' />
         </div>
       </div>
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full px-3">
           <label className="block uppercase tracking-wide text-violeta-ciba text-xs font-bold mb-2" htmlFor="grid-password">
-            E-mail
+            E-mail *
           </label>
-          <input className="appearance-none block w-full bg-violet-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="pepita@lapistolera.com" name='email' />
+          <input required className="appearance-none block w-full bg-violet-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="pepita@lapistolera.com" name='email' />
         </div>
       </div>
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full px-3">
           <label className="block uppercase tracking-wide text-violeta-ciba text-xs font-bold mb-2" htmlFor="grid-password">
-            Edad ñiña/niño
+            Edad ñiña/niño *
           </label>
-          <select className=" block w-full bg-violet-100 text-grey-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" onChange={changeSelectOptionHandler}>
+          <select required className=" block w-full bg-violet-100 text-grey-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" onChange={changeSelectOptionHandler}>
             <option>Elegir edad</option>
             <option>0-6</option>
             <option>7-13</option>
@@ -93,15 +90,15 @@ function Form() {
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label className="block uppercase tracking-wide text-violeta-ciba text-xs font-bold mb-2" >
-            Fecha
+            Fecha *
           </label>
-          <input className="appearance-none block w-full bg-violet-100 text-grey-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="date" name='fecha' />
+          <input required className="appearance-none block w-full bg-violet-100 text-grey-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="date" name='fecha' />
         </div>
         <div className="w-full md:w-1/2 px-3">
           <label className="block uppercase tracking-wide text-violeta-ciba text-xs font-bold mb-2" htmlFor="grid-last-name">
-            Horario
+            Horario *
           </label>
-          <select className=" block w-full bg-violet-100 text-grey-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" name='horario'>
+          <select required className=" block w-full bg-violet-100 text-grey-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" name='horario'>
             {options}
           </select>
         </div>
@@ -112,11 +109,13 @@ function Form() {
             Información Complementaria
           </label>
           <textarea className=" no-resize appearance-none block w-full bg-violet-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48" id="message" name='info complementaria'></textarea>
+          <p class="text-violeta-ciba text-sm italic">* = Campos obligatorios</p>
         </div>
       </div>
+      
       <div className="md:flex md:items-center">
         <div className="md:w-1/3">
-          <button className="shadow bg-violeta-ciba transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-verde-ciba duration-300 focus:shadow-outline focus:outline-none text-white font-koulen py-2 px-4 rounded" type="submit">
+          <button onClick={handleClick} className="shadow bg-violeta-ciba transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-verde-ciba duration-300 focus:shadow-outline focus:outline-none text-white font-koulen py-2 px-4 rounded" type="submit">
             Reservar
           </button>
         </div>
