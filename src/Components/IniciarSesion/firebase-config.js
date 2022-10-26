a7c91106a11b8cefc39bd7afee50c32f9575b27e
Nika <nikaguisasola@gmail.com>
2022-10-26 11:40:18 +0200

    menudaciba_database_ok

--- src/Components/IniciarSesion/firebase-config.js
+++ src/Components/IniciarSesion/firebase-config.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIKhxk8rTEI876jlG-xa133W5YvuaTFWM",
-  authDomain: "menuda-ciba.firebaseapp.com",
-  projectId: "menuda-ciba",
-  storageBucket: "menuda-ciba.appspot.com",
-  messagingSenderId: "641909532183",
-  appId: "1:641909532183:web:5c85bcad1b8620bc925e52",
-  measurementId: "G-J2T98D20MQ"
+  apiKey: "AIzaSyBX3OMOQdwvPtorpBhdt6v-NGe81mIBTjg",
+  authDomain: "menudaciba-7dabc.firebaseapp.com",
+  projectId: "menudaciba-7dabc",
+  storageBucket: "menudaciba-7dabc.appspot.com",
+  messagingSenderId: "280192237555",
+  appId: "1:280192237555:web:71bc6a2957409effab9c86",
+  measurementId: "G-80WXD2ZHVV"
 };
 
 export const app = initializeApp(firebaseConfig);
