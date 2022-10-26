d1a5705d6d4170457e18699cba5d219a027d61a5
mflamanna <lamanna.mflorencia@gmail.com>
2022-10-26 11:51:50 +0200

    callbell menudaciba

--- src/Components/Team/ContainerTeam.jsx
+++ src/Components/Team/ContainerTeam.jsx
@@ -4,10 +4,10 @@ import TeamMember from './TeamMember'
 function ContainerTeam() {
   return (
     <div className='flex flex-wrap justify-center items-center'>
-        <TeamMember nombre="Laura"/>
-        <TeamMember nombre="Pedro"/>
-        <TeamMember nombre="Artemisia"/>
-        <TeamMember nombre="Louis"/>
+        <TeamMember nombre="Laura" foto="https://i.postimg.cc/vB5VjzdZ/team1.jpg"/>
+        <TeamMember nombre="Victoria" foto="https://i.postimg.cc/FzRhcrGX/team2.jpg"/>
+        <TeamMember nombre="María" foto="https://i.postimg.cc/k5vCp6pd/team3.jpg"/>
+        <TeamMember nombre="Paula" foto="https://i.postimg.cc/fWrNphBk/team4.jpg"/>
         
     </div>
  
--- src/Components/Team/TeamMember.jsx
+++ src/Components/Team/TeamMember.jsx
@@ -4,7 +4,7 @@ function TeamMember(props) {
   return (
     <div className='flex flex-col justify-center items-center p-4'>
         <div className='w-48 h-48 rounded-full bg-violeta-ciba'>
-            <img className="clip-mask" src="https://thispersondoesnotexist.com/image" alt="Team Member" />
+            <img className="clip-mask" src={props.foto} alt="Team Member" />
         </div>
         <p className='font-alata text-violeta-ciba text-2xl'>{props.nombre}</p>
     </div>
